78f32efaaeeba090fb2b917a69a29418fbf5876c
yiping999 <yiming139@163.com>
2017-09-11 15:40:37 +0800

    add spring boot admin

---
+++ admin-service/src/main/java/com/piggymetrics/admin/SpringBootAdminApplication.java
@@ -0,0 +1,22 @@
+package com.piggymetrics.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+
+// tag::application-eureka[]
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
+// end::application-eureka[]
