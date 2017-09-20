d77a9c649eba1fbce1baf11cfe966b6b1940f7f0
yiping999 <yiming139@163.com>
2017-09-20 15:00:28 +0800

    updated

--- customer-service/src/main/java/com/piggymetrics/customer/controller/CustomerController.java
+++ customer-service/src/main/java/com/piggymetrics/customer/controller/CustomerController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,37 +17,37 @@ import com.piggymetrics.customer.domain.Customer;
 import com.piggymetrics.customer.domain.User;
 import com.piggymetrics.customer.service.CustomerService;
 
-//@Api(tags="用户管理")
-@RestController
-public class CustomerController {
-	
-	private final Logger log = LoggerFactory.getLogger(getClass());
-
-	@Autowired
-	private CustomerService customerService;
-	
-	
-//	  @ApiOperation("用户详情")
-	//@PreAuthorize("#oauth2.hasScope('server') or #name.equals('demo') or #name.equals('yiping')")
-	@RequestMapping(path = "/{name}", method = RequestMethod.GET)
-	public Customer getCustomerByName(@PathVariable String name) {
-		
-		log.info("get customer: " + name);
-		return customerService.findByName(name);
-	}
-
-	@RequestMapping(path = "/current", method = RequestMethod.GET)
-	public Customer getCurrentCustomer(Principal principal) {
-		return customerService.findByName(principal.getName());
-	}
-
-	@RequestMapping(path = "/current", method = RequestMethod.PUT)
-	public void saveCurrentCustomer(Principal principal, @Valid @RequestBody Customer customer) {
-		customerService.saveChanges(principal.getName(), customer);
-	}
-
-	@RequestMapping(path = "/", method = RequestMethod.POST)
-	public Customer createNewCustomer(@Valid @RequestBody User user) {
-		return customerService.create(user);
-	}
-}
+////@Api(tags="用户管理")
+//@RestController
+//public class CustomerController {
+//	
+//	private final Logger log = LoggerFactory.getLogger(getClass());
+//
+//	@Autowired
+//	private CustomerService customerService;
+//	
+//	
+////	  @ApiOperation("用户详情")
+//	//@PreAuthorize("#oauth2.hasScope('server') or #name.equals('demo') or #name.equals('yiping')")
+//	@RequestMapping(path = "/{name}", method = RequestMethod.GET)
+//	public Customer getCustomerByName(@PathVariable String name) {
+//		
+//		log.info("get customer: " + name);
+//		return customerService.findByName(name);
+//	}
+//
+//	@RequestMapping(path = "/current", method = RequestMethod.GET)
+//	public Customer getCurrentCustomer(Principal principal) {
+//		return customerService.findByName(principal.getName());
+//	}
+//
+//	@RequestMapping(path = "/current", method = RequestMethod.PUT)
+//	public void saveCurrentCustomer(Principal principal, @Valid @RequestBody Customer customer) {
+//		customerService.saveChanges(principal.getName(), customer);
+//	}
+//
+//	@RequestMapping(path = "/", method = RequestMethod.POST)
+//	public Customer createNewCustomer(@Valid @RequestBody User user) {
+//		return customerService.create(user);
+//	}
+//}
